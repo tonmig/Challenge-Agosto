49ca0879076bad420c40d468170eaaf9d4a8c956
tonmig <tony.abe06@gmail.com>
2025-08-11 21:06:35 -0600

    ya termine la primer fase de la funcion

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = []; // Arreglo para almacenar los nombres de los amigos
+
+function agregarAmigos(){
+     // Obtener el valor del campo de entrada
+    const input = document.getElementById("amigo");
+    const nombre = input.value.trim();
+    // Verificar que el campo no esté vacío
+    if (nombre ===""){
+        alert("por favor ingresa un nombre valido");
+        return
+    }
+      // Agregar el nombre al arreglo
+    amigos.push(nombre);
+      // Limpiar el campo de entrada
+    input.value = "";
+
+      // Mostrar la lista actualizada en el HTML
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = ""; // Limpiar la lista antes de actualizarla
+    amigos.forEach((amigo) => {
+        const li = document.createElement("li");
+        li.textContent = amigo;
+        lista.appendChild(li);
+    });
+
+}
+
+
+
